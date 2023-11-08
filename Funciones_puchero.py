687963a5377fca182b46934019814eeefb4785d9
Juan <juca.acuna@gmail.com>
2023-11-08 08:00:05 -0300

    Se agrega función.

---
+++ Funciones_puchero.py
@@ -0,0 +1,10 @@
+from microbit import *
+
+
+def puchero():
+    puchero=["choclo", "zanahoria", "cebolla", "papa", "boniato", "zapallo", "sal"]
+    display.scroll(str(puchero))  
+    
+    
+    
+puchero()
